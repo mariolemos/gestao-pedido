8bcc154cfdc5298a25322ae0386328e6bc61c3e8
mariolemos <lemosmm64@gmail.com>
2026-08-18 15:47:15 -0300

    Projeto Criado

---
+++ src/main/java/com/mariolemos/gestao_pedido/gestao_pedido/GestaoPedidoApplication.java
@@ -0,0 +1,14 @@
+package com.mariolemos.gestao_pedido.gestao_pedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPedidoApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/mariolemos/gestao_pedido/gestao_pedido/GestaoPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mariolemos.gestao_pedido.gestao_pedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
